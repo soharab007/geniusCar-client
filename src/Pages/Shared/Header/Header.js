20122f67ba7f68840959729ef1d4ada8bd33d807
soharab007 <soharabhossain943@gmail.com>
2023-02-25 08:39:41 +0600

    orders section allmost done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.svg";
+import { AuthContext } from "../../../Contexts/AuthProvider/AuthProvider";
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
   const menuItems = (
     <>
       <li className="font-semibold">
         <Link to="/">Home</Link>
       </li>
-      <li className="font-semibold">
-        <Link to="/login">Login</Link>
-      </li>
+      {user?.email ? (
+        <>
+          <li className="font-semibold">
+            <Link to="/orders">Orders</Link>
+          </li>
+        </>
+      ) : (
+        <li className="font-semibold">
+          <Link to="/orders">Orders</Link>
+        </li>
+      )}
     </>
   );
   return (
---
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return <h3 className="text-5xl"> loading ...</h3>;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
